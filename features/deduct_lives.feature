3664b0ac13da1c33170da64007e8298fdd9fabf7
randomcodenz <neal.blomfield@gmail.com>
2016-09-08 16:38:29 +1200

    Confirm the game to deducts a life for incorrect guesses
    Added feature to confirm that the correct number of lives is displayed after each of the following:
     - incorrect guess
     - invalid guess
     - correct guess
     - duplicate guess
    
    Logic to correctly calculate this was already present - t was implemented when implementing display lives remaining

--- features/deduct_lives.feature
+++ features/deduct_lives.feature
@@ -1,1 +1,23 @@
-Feature: 8. As a hangman player, there should be 1 life deducted every time I make an incorrect guess of a letter that I have not already guessed
+Feature: Deduct a live for an incorrect guess
+  As a hangman player, there should be 1 life deducted every time I make an
+  incorrect guess of a letter that I have not already guessed
+
+  Scenario: Deduct life for incorrect guess
+    Given I have started a game with the word "xyzzy" and 5 lives
+    When I make an incorrect guess ("w")
+    Then I should end up with 4 lives
+
+  Scenario: Do not deduct a life for an invalid guess
+    Given I have started a game with the word "xyzzy" and 5 lives
+    When I make an invalid guess ("@")
+    Then I should still have 5 lives
+
+  Scenario: Do not deduct a life for a correct guess
+    Given I have started a game with the word "xyzzy" and 5 lives
+    When I make a valid guess ("x")
+    Then I should still have 5 lives
+
+  Scenario: Do not deduct a life for a duplicate guess
+    Given I have started a game with the word "xyzzy" and 5 lives
+    When I make a valid guess ("x") twice
+    Then I should still have 5 lives
---
+++ features/step_definitions/deduct_lives_steps.rb
@@ -0,0 +1,44 @@
+Given(/^I have started a game with the word "([^"]*)" and (\d+) lives$/) do |word, initial_lives|
+  Game.create(:word => word, :initial_lives => initial_lives)
+  visit game_path(Game.last)
+end
+
+When(/^I make an incorrect guess \("([^"]*)"\)$/) do |guess|
+  within('form#guess') do
+    fill_in('Attempt', :with => guess)
+    click_button
+  end
+end
+
+Then(/^I should end up with (\d+) lives$/) do |lives_remaining|
+  expect(find('span#lives_remaining').text).to eq lives_remaining.to_s
+end
+
+When(/^I make an invalid guess \("([^"]*)"\)$/) do |guess|
+  within('form#guess') do
+    fill_in('Attempt', :with => guess)
+    click_button
+  end
+end
+
+Then(/^I should still have (\d+) lives$/) do |lives_remaining|
+  expect(find('span#lives_remaining').text).to eq lives_remaining.to_s
+end
+
+When(/^I make a valid guess \("([^"]*)"\)$/) do |guess|
+  within('form#guess') do
+    fill_in('Attempt', :with => guess)
+    click_button
+  end
+end
+
+When(/^I make a valid guess \("([^"]*)"\) twice$/) do |guess|
+  within('form#guess') do
+    fill_in('Attempt', :with => guess)
+    click_button
+  end
+  within('form#guess') do
+    fill_in('Attempt', :with => guess)
+    click_button
+  end
+end

cda379ffd1a31e4e0ba28f377178351b38bc5ec9
randomcodenz <neal.blomfield@gmail.com>
2016-09-13 20:48:09 +1200

    Applied review feedback to routes
    Removed the unnecessary route
    Restricted the routes on game to those that are used

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-  # REVIEW: Not required unless specifically referenced
-  get 'welcome/index'
-
   root 'welcome#index'
 
-  # REVIEW: Restrict actions for game to only those that are used
-  resources :games do
+  resources :games, only: [:create, :show] do
     resources :guesses, only: [:create]
   end
 end

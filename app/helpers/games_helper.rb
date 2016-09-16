0381ccfb5b44a1b904f2bdb98e0287808e8c0447
randomcodenz <neal.blomfield@gmail.com>
2016-09-16 13:16:17 +1200

    Made show template render in a declarative manner
    Extracted all of the conditionally rendered parts of the template out into partials
    Create a class that either renders the template or skips rendering
    Rewrote the helper to use the new conditional rendering approach moving all of the conditionals out of the view

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,13 +1,111 @@
 module GamesHelper
-  def render_masked_word(game)
-    game.masked_letters
+  def render_masked_word(masked_letters)
+    masked_letters
       .map { |masked_letter| render_letter(masked_letter) }
       .join(' ')
+  end
+
+  def guess_attempt(guess_attempt)
+    if guess_attempt.nil?
+      MaybeTemplate.new()
+    else
+      MaybeTemplate.new('guess_attempt', guess_attempt)
+    end
+  end
+
+  def guess_result(guess_correct, guess_attempt)
+    if guess_correct.nil?
+      MaybeTemplate.new()
+    else
+      guess_result_template = guess_correct ? 'guess_correct' : 'guess_incorrect'
+      {:partial => guess_result_template, :locals => { :guess_attempt => guess_attempt } }
+    end
+  end
+
+  def incorrect_guesses(incorrect_guesses)
+    if incorrect_guesses.any?
+      MaybeTemplate.new('incorrect_guesses', incorrect_guesses)
+    else
+      MaybeTemplate.new()
+    end
+  end
+
+  def game_over(game)
+    if game.game_over?
+      template = game.game_won? ? 'game_won' : 'game_lost'
+      { :partial => MaybeTemplate.new(template), :locals => { :word => game.word } }
+    else
+      MaybeTemplate.new()
+    end
+  end
+
+  def game_in_progress(game)
+    if game.game_over?
+      MaybeTemplate.new()
+    else
+      { :partial => MaybeTemplate.new('game_in_progress'), :locals => { :game => game } }
+    end
   end
 
   private
 
   def render_letter(masked_letter)
     masked_letter.nil? ? '_' : masked_letter
+  end
+
+  # TODO: Needs better names - could probably use the outer class as an eigenclass
+  # TODO: to hold a method for creating the desired instance (None | Some)
+  # TODO: None is more like SkipRendering and Some is more like DoRendering
+  class MaybeTemplate
+  	def MaybeTemplate.new(*args)
+  		if args.empty?
+  			None.new()
+  		else
+  			Some.new(*args)
+  		end
+  	end
+
+	   private
+
+  	class None
+      # TemplateRenderer will :respond_to? this so we can abuse that and return nil
+      def render
+      end
+
+      # PartialRenderer will :respond_to? this to switch into collection rendering
+      # mode so abuse that and return an empty array
+      def to_ary
+        []
+  		end
+  	end
+
+  	class Some
+      extend Forwardable
+
+      # This object will be create as a local with the same name as the
+      # template so forward all calls to the object
+      def initialize(template, object=nil)
+  			@template = template
+        @object = object
+        delegate_to = delegated_methods
+        (class << self; self; end).class_eval do
+          def_delegators :'@object', *delegate_to
+        end
+  		end
+
+      # Renderers use this to determine the partial to render
+      def to_partial_path
+  			@template
+  		end
+
+      private
+
+      # PartialTemplateRenderer uses respond_to? :to_ary to switch into collection rendering
+      # mode (which we don't want) so exclude it from the set of forwarded methods
+      # If you want collection rendering - you don't need MaybeTemplate ;)
+      def delegated_methods
+        @object.public_methods(false).reject { |method| method == :to_ary }
+      end
+  	end
   end
 end
--- spec/helpers/games_helper_spec.rb
+++ spec/helpers/games_helper_spec.rb
@@ -1,15 +1,67 @@
 require 'rails_helper'
+require 'spec_helper'
 
 describe GamesHelper, type: :helper do
   describe '#render_masked_word' do
-    let(:word) { 'Powershop' }
+    context 'when no letters are guessed' do
+      let(:masked_letters) { [nil, nil, nil] }
 
-    context "when no letters are guessed" do
-      let(:game) { Game.new( { :word => word, :initial_lives => 5} ) }
-
-      it 'renders all of the nils as underscores' do
-        expect(helper.render_masked_word(game)).to eq word.chars.map { '_' }.join(' ')
+      it 'renders the nils as underscores' do
+        expect(helper.render_masked_word(masked_letters)).to eq '_ _ _'
       end
     end
+
+    context 'when some letters are guessed' do
+      let(:masked_letters) { [nil, 'h', nil] }
+
+      it 'renders the letters and replaces the nils with underscores' do
+        expect(helper.render_masked_word(masked_letters)).to eq '_ h _'
+      end
+    end
+
+    context 'when all of the letters are guessed' do
+      let(:masked_letters) { ['t', 'h', 'e'] }
+
+      it 'renders the letters' do
+        expect(helper.render_masked_word(masked_letters)).to eq 't h e'
+      end
+    end
+  end
+
+  describe '#guess_attempt' do
+    it 'returns MaybeTemplate::None if guess_attempt is nil' do
+      expect(helper.guess_attempt(nil)).to be_nil
+    end
+
+    context 'when guess_attempt is not nil' do
+      before {  }
+
+      it 'renders the guess_attempt partial' do
+        skip "Developer broken attempting to rspec"
+        expect(helper.render_guess_attempt('h')).to have_rendered('guess_attempt')
+      end
+    end
+  end
+
+  describe '#render_guess_result' do
+    it 'returns nil if guess_correct is nil'
+    it 'renders the guess_correct partial if guess_correct is true'
+    it 'renders the guess_incorrect partial if guess_correct is false'
+  end
+
+  describe '#render_incorrect_guesses' do
+    it 'returns nil if incorrect_guesses is empty'
+    it 'renders the incorrect_guesses partial if incorrect_guesses is not empty'
+  end
+
+  describe '#render_game_over' do
+    it 'returns nil if game_over? is false'
+    it 'renders the game_won partial if game_won? is true'
+    it 'renders the game_lost partial if game_lost? is true'
+  end
+
+  describe '#render_game_in_progress' do
+    it 'returns nil if game_over? is true'
+    it 'renders the game_in_progress partial if game_over? is false'
   end
 end
